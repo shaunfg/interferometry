2c67437ac7396e5832eb72a438c167591dd36c7e
shaunfendigan@gmail.com <shaunfendigan@gmail.com>
2019-01-26 15:38:29 +0000

    Updates on coding progress
    Fit gaussian fit successfully
    attempted doing the beating pattern but unable to find the right params, fitted a 2 cos curves and an exp to create decaying beating pattern.

--- Plots/gaussian_fit.py
+++ Plots/gaussian_fit.py
@@ -7,10 +7,13 @@ Created on Tue Jan 22 14:03:28 2019
 
 import numpy as np
 import matplotlib.pyplot as plt
-from scipy.signal import hilbert,chirp
+from scipy.signal import hilbert,butter,filtfilt,chirp
+from scipy.optimize import curve_fit
+#import random
 
 # Define some test data which is close to Gaussian
-fname = 'white_light.txt'
+#fname = 'blue_LED.txt'
+fname = 'green_tungsten.txt'
 
 f = open(fname,'r')
 
@@ -18,7 +21,6 @@ signal=[]
 t_sec=[]
 t_usec=[]
 position=[]
-
 
 #read in the data
 lines=f.readlines()
@@ -28,17 +30,132 @@ for line in range(len(lines)):
     t_usec.append(lines[line].split()[2])
     position.append(lines[line].split()[3])
 
-#data = random.sample(x,5)
 
-x = np.array(position)
-y = np.array(signal)
+position = np.array(position)
+position = position.astype(float)
 
-duration = 1.0
-fs = 400.0
-samples = int(fs*duration)
-t = np.arange(samples) / fs
+signal = np.array(signal)
 
-signal = chirp(t, 20.0, t[-1], 100.0)
-signal *= (1.0 + 0.5 * np.sin(2.0*np.pi*3.0*t) )
+analytic_signal = hilbert(signal,axis= -1)
 
-plt.plot(x,y)
+#plt.plot(np.real(analytic_signal))
+analytic_imag_signal = hilbert((np.imag(analytic_signal)))
+amplitude_envelope = np.abs(analytic_imag_signal)
+
+plt.figure(figsize = [13,8])
+plt.plot(position,np.imag(analytic_signal)) #plots the curve along the y axis
+plt.plot(position,amplitude_envelope)
+plt.plot(position,-amplitude_envelope)
+
+y = amplitude_envelope
+x = position
+
+#%% beating fit
+y = amplitude_envelope
+x = position
+
+x2 = np.array([i for i in x if i >= 0.63])
+y = np.array(y[:len(x2)])
+
+def beating (t,amplitude,period,period_del,phase,tau):
+    func = amplitude*(np.cos(t * 2. * np.pi/period + phase)*
+                      np.cos(t * 2. * np.pi/period_del + phase))* np.exp(-t/tau)
+    return func
+
+guess_amplitude = 20
+guess_period = 0.470015001032
+guess_period_del = 0.001
+guess_phase = 0.5
+guess_tau = 1/20
+
+p0 = [guess_amplitude,guess_period,guess_period_del,guess_phase,guess_tau]
+
+fit = curve_fit (beating,x2,y,p0=p0,maxfev = 10000)
+
+data_fit= beating(x2 , *fit[0])
+plt.figure(figsize = [13,8])
+plt.plot(x,np.imag(analytic_signal),label='data') #plots the curve along the y axis
+plt.plot (x2, 20*data_fit,'r:')
+
+#%% beating fit with different range ... takes a bit longer to load
+y = amplitude_envelope
+x = position
+
+x2 = np.array([i for i in x if i >= 0.63])
+x2 = np.array([i for i in x2 if i <= 0.67])
+y = np.array(y[:len(x2)])
+
+def beating (t,amplitude,period,period_del,phase,tau):
+    func = amplitude*(np.cos(t * 2. * np.pi/period + phase)*
+                      np.cos(t * 2. * np.pi/period_del + phase))* np.exp(-t/tau)
+    return func
+
+guess_amplitude = 6000
+guess_period = 0.054
+guess_period_del = 0.0021
+guess_phase = 0.5
+guess_tau = 1/20
+
+p0 = [guess_amplitude,guess_period,guess_period_del,guess_phase,guess_tau]
+
+fit = curve_fit (beating,x2,y,p0=p0,maxfev = 100000)
+
+data_fit= beating(x2 , *fit[0])
+plt.plot(x,np.imag(analytic_signal),label='data') #plots the curve along the y axis
+plt.plot (x2, 300*data_fit,'r:')
+
+
+#%%
+#Gaussian Fit
+
+y = amplitude_envelope
+x = position
+
+n = len(x)
+mean = sum(x)/n
+sigma = np.sqrt(sum((x-mean)**2)/n)
+
+#Test Function
+def gaus(x,a,x0,sigma):
+    return a*np.exp(-(x-x0)**2/(2*sigma**2))
+
+popt,pcov = curve_fit(gaus,x,y,p0=[1,mean,sigma],maxfev=5000)
+plt.figure()
+plt.plot(x,np.imag(analytic_signal),label='data') #plots the curve along the y axis
+plt.plot(x,gaus(x,*popt),'r',label='fit')
+plt.legend()
+
+#%%
+
+a = [1,2,3,4,5]
+
+print(a[-3:])
+
+#%%
+ import numpy as np
+ import matplotlib.pyplot as plt
+ from scipy.signal import hilbert, chirp
+
+ duration = 1.0
+ fs = 1000.0
+ samples = int(fs*duration)
+ t = np.arange(samples) / fs
+
+ signal = chirp(t, 20.0, t[-1], 100.0)
+ signal *= (1.0 + 0.5 * np.sin(2.0*np.pi*3.0*t) )
+
+ analytic_signal = hilbert(signal)
+ amplitude_envelope = np.abs(analytic_signal)
+ instantaneous_phase = np.unwrap(np.angle(analytic_signal))
+ instantaneous_frequency = np.diff(instantaneous_phase) / (2.0*np.pi) * fs
+
+ fig = plt.figure()
+ ax0 = fig.add_subplot(211)
+ ax0.plot(t, signal, label='signal')
+ ax0.plot(t, amplitude_envelope, label='envelope')
+ ax0.set_xlabel("time in seconds")
+ ax0.legend()
+ ax1 = fig.add_subplot(212)
+ ax1.plot(t[1:], instantaneous_frequency)
+ ax1.set_xlabel("time in seconds")
+ ax1.set_ylim(0.0, 120.0)
